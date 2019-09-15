52ca679f108fa991c239f988a9f90d3f0c816e27
rashad <rashadulkabir@gmail.com>
2019-09-15 21:25:57 +0600

    Commit with deepcopy Error

--- ImplementationSA.py
+++ ImplementationSA.py
@@ -44,7 +44,7 @@ class ImplementationSA():
 
         #Calculate task order
         returnlist = []
-
+        itr = 1
         task_order = [0]
         temp_list = [0]
         while len(temp_list) > 0:
@@ -71,22 +71,22 @@ class ImplementationSA():
             all_exec_time.append(str("%.2f" % (self.g.total_execution_time/1000)) + 'seconds')
 
             #prntString = ""
+            itr = itr + 1
+        lines.append('This file contains all the possible combination of processors with their consumed energy and execution time\n')
+        lines.append('Speed of each processor is given in a row. "0" indicates and unused processor.\n\n')
+        line = ''
+        for j in range(self.g.num_processors):
+            line += 'P' + str(j) + ','
+        line += 'Energy and Execution time\n'
+        lines.append(line)
 
-            lines.append('This file contains all the possible combination of processors with their consumed energy and execution time\n')
-            lines.append('Speed of each processor is given in a row. "0" indicates and unused processor.\n\n')
-            line = ''
-            for j in range(self.g.num_processors):
-                line += 'P' + str(j) + ','
-            line += 'Energy and Execution time\n'
+        for j in range(len(all_speeds)):
+            line = ','.join(["%s" % elem for elem in all_speeds[j]])+','+str(all_energies[j])+','+str(all_exec_time[j]) +'\n'
             lines.append(line)
 
-            for j in range(len(all_speeds)):
-                line = ','.join(["%s" % elem for elem in all_speeds[j]])+','+str(all_energies[j])+','+str(all_exec_time[j]) +'\n'
-                lines.append(line)
-
-            file = open(self.path,'w')
-            file.writelines(lines)
-            file.close()
+        file = open(self.path,'w')
+        file.writelines(lines)
+        file.close()
 
     def findTaskOrder(self,g,temp_list,task_order):
         temp = []
